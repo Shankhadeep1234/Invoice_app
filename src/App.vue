24fb0ba22e3129a5ebf8b78ab99ab303ade7b4ad
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-01-15 14:03:47 +0530

    changed the title

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      //
+    };
+  },
+  mounted() {
+    document.title = 'Invoice App'
+  }
+}
+</script>
+
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
 
